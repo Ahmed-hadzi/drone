6439cfe1a9850d5abeff717c370d021547b0c606
Ahmed Hadziomerovic <91010342+Ahmed-hadzi@users.noreply.github.com>
2025-03-31 19:05:41 +0200

    servo release

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,9 +1,20 @@
+#include <Servo.h>
+#define campin 12
+
+Servo camservo;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(12, OUTPUT);
+  camservo.attach(campin, 500, 2500);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for(int  i = 2500; i>500;i=i-10){
+    camservo.writeMicroseconds(i);
+    delay(50);
+  }
 
 }
